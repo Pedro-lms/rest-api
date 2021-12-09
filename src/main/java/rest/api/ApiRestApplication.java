e0f1048f498e12f3f1e8e86845bb5bba0e19c509
Pedro-lms <pedrodaluzmachado@gmail.com>
2021-12-09 14:33:07 -0300

    Rest API project initial setup and its configs

---
+++ src/main/java/rest/api/ApiRestApplication.java
@@ -0,0 +1,29 @@
+package rest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EntityScan(basePackages = {"rest.api.model"})
+@ComponentScan(basePackages = {"rest.api.*"})
+@EnableJpaRepositories(basePackages = {"rest.api.repository"})
+@EnableTransactionManagement
+@EnableWebMvc
+@RestController
+@EnableAutoConfiguration
+@EnableAsync
+public class ApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestApplication.class, args);
+	}
+
+}
